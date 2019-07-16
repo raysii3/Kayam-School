415be6b99e9b64c81e90b7a1d5279a1acfc2775e
akashdabhi03 <akashd@maqsoftware.com>
2019-07-16 13:14:00 +0530

    Updated book resource path (#196)

--- bookviewer/Classes/AppDelegate.cpp
+++ bookviewer/Classes/AppDelegate.cpp
@@ -4,25 +4,21 @@
 #include "Common/Controls/TodoLoadingScene.hpp"
 #include "Managers/LanguageManager.hpp"
 #include "Utils/TodoUtil.h"
-USING_NS_CC;
 
+USING_NS_CC;
 
 static cocos2d::Size macResolutionSize = cocos2d::Size(1024, 720);
 static cocos2d::Size designResolutionSize = cocos2d::Size(2560, 1800);
 
-
 AppDelegate::AppDelegate() {
-
 }
 
-AppDelegate::~AppDelegate() 
-{
+AppDelegate::~AppDelegate() {
 }
 
 //if you want a different context,just modify the value of glContextAttrs
 //it will takes effect on all platforms
-void AppDelegate::initGLContextAttrs()
-{
+void AppDelegate::initGLContextAttrs() {
     //set OpenGL context attributions,now can only set six attributions:
     //red,green,blue,alpha,depth,stencil
     GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
@@ -32,13 +28,9 @@ void AppDelegate::initGLContextAttrs()
 
 // If you want to use packages manager to install more packages, 
 // don't modify or remove this function
-static int register_all_packages()
-{
+static int register_all_packages() {
     return 0; //flag for packages manager
 }
-
-
-
 
 
 bool AppDelegate::applicationDidFinishLaunching() {
@@ -47,7 +39,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     // initialize director
     auto director = Director::getInstance();
     auto glview = director->getOpenGLView();
-    if(!glview) {
+    if (!glview) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
         glview = GLViewImpl::createWithRect("booktest", Rect(0, 0, macResolutionSize.width, macResolutionSize.height));
 #else
@@ -60,38 +52,36 @@ bool AppDelegate::applicationDidFinishLaunching() {
     director->setDisplayStats(false);
 
     // set FPS. the default value is 1.0/60 if you don't call this
-    director->setAnimationInterval(1.0 / 60);
+    director->setAnimationInterval(static_cast<float>(1.0 / 60));
 
     // Set the design resolution
-    glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::FIXED_HEIGHT);
-    
+    glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height,
+                                    ResolutionPolicy::FIXED_HEIGHT);
+
     register_all_packages();
-    
+
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
-    std::string devicePath = "/storage/emulated/0/Android/data/com.maq.pehlaschool/files/";
-    FileUtils::getInstance()->setDefaultResourceRootPath(devicePath);
+
+    // Extracted content path for Hindi
+    std::string extractedContentPathHindi = "/storage/emulated/0/Android/data/com.maq.pehlaschool/files/";
+    std::string extractedContentPathUrdu = "/storage/emulated/0/Android/data/com.maq.pehlaschool.urdu/files/";
+
+    // Set resource path based on available resource directory
+    if (FileUtils::getInstance()->isDirectoryExist(extractedContentPathHindi)) {
+        FileUtils::getInstance()->setDefaultResourceRootPath(extractedContentPathHindi);
+    } else if (FileUtils::getInstance()->isDirectoryExist(extractedContentPathUrdu)) {
+        FileUtils::getInstance()->setDefaultResourceRootPath(extractedContentPathUrdu);
+    }
+
 
 #endif
 
-    Scene* scene;
-    /*
-    auto book= MainScene::getLaunchString();
-    //if (book.length()>0) {
-    //    scene = HelloWorld::createBookScene(book);
-    //} else
-    
-    {
+    Scene *scene;
+
+    if (!launchBook()) {
         scene = MainScene::createScene();
+        director->runWithScene(scene);
     }
-
-    // run
-    director->runWithScene(scene);
-    */
-
-	if (!launchBook()) {
-		scene = MainScene::createScene();
-		director->runWithScene(scene);
-	}
 
     return true;
 }
@@ -99,64 +89,50 @@ bool AppDelegate::applicationDidFinishLaunching() {
 // This function will be called when the app is inactive. When comes a phone call,it's be invoked too
 void AppDelegate::applicationDidEnterBackground() {
     Director::getInstance()->stopAnimation();
-    //Director::getInstance()->pause();
     // if you use SimpleAudioEngine, it must be pause
     //CocosDenshion::SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
-
-
 }
 
 // this function will be called when the app is active again
 void AppDelegate::applicationWillEnterForeground() {
     CCLOG("applicationWillEnterForeground()");
 
-//    Director::getInstance()->resume();
     Director::getInstance()->startAnimation();
 
     // if you use SimpleAudioEngine, it must resume here
     //CocosDenshion::SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
-    
-    
-	launchBook();
+
+    launchBook();
 }
 
-bool AppDelegate::launchBook()
-{
-	auto book = MainScene::getLaunchString();
-	CCLOG("argument = [%s]", book.c_str());
-	if (book.length() > 0 && book != MainScene::currentBook) {
-		MainScene::clearLaunchString();
-		MainScene::currentBook = book;
+bool AppDelegate::launchBook() {
+    auto book = MainScene::getLaunchString();
+    CCLOG("argument = [%s]", book.c_str());
+    if (book.length() > 0 && book != MainScene::currentBook) {
+        MainScene::clearLaunchString();
+        MainScene::currentBook = book;
 
+        auto folderName = TodoUtil::split(book, '/').back();
+        if (folderName.find("en") == 0) {
+            LanguageManager::getInstance()->setCurrentLocale(LanguageManager::LocaleType::en_US);
+        } else if (folderName.find("sw") == 0) {
+            LanguageManager::getInstance()->setCurrentLocale(LanguageManager::LocaleType::sw_TZ);
+        }
 
-		auto folderName = TodoUtil::split(book, '/').back();
-		if (folderName.find("en") == 0) {
-			LanguageManager::getInstance()->setCurrentLocale(LanguageManager::LocaleType::en_US);
-		}
-		else if (folderName.find("sw") == 0) {
-			LanguageManager::getInstance()->setCurrentLocale(LanguageManager::LocaleType::sw_TZ);
-		}
+        std::function<Scene *(void)> creator = [book]() {
+            auto scene = MainScene::createBookScene(book);
+            return scene;
+        };
 
+        auto running = Director::getInstance()->getRunningScene();
+        if (running) {
+            Director::getInstance()->replaceScene(TodoLoadingScene::createScene(creator));
+        } else {
+            Director::getInstance()->runWithScene(TodoLoadingScene::createScene(creator));
+        }
 
-		std::function<Scene*(void)> creator = [book]() {
-			auto scene = MainScene::createBookScene(book);
-			return scene;
-		};
+        return true;
+    }
 
-
-		auto running = Director::getInstance()->getRunningScene();
-		if (running) {
-			Director::getInstance()->replaceScene(TodoLoadingScene::createScene(creator));
-		}
-		else {
-			Director::getInstance()->runWithScene(TodoLoadingScene::createScene(creator));
-		}
-
-		return true;
-	}
-
-
-
-
-	return false;
+    return false;
 }
