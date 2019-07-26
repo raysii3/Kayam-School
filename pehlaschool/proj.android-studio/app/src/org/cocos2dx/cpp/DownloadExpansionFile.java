985cc3e9f1295a9be00a43563751e16fa9524a65
akashdabhi03 <akashd@maqsoftware.com>
2019-07-26 23:35:01 +0530

    Updated Learning module's OBB file size (#249)

--- pehlaschool/proj.android-studio/app/src/org/cocos2dx/cpp/DownloadExpansionFile.java
+++ pehlaschool/proj.android-studio/app/src/org/cocos2dx/cpp/DownloadExpansionFile.java
@@ -46,7 +46,7 @@ public class DownloadExpansionFile extends Activity implements IDownloaderClient
             new XAPKFile(
                     true, // true signifies a main file
                     6, // the version of the APK that the file was uploaded
-                    1276815732L // the length of the file in bytes
+                    1283809280L // the length of the file in bytes
             ),
             new XAPKFile(
                     false, // true signifies a main file
