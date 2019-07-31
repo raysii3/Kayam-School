4cd2684e5908570cb98e2514a778dd29df714c14
bikashtudu <bikashtudu@outlook.com>
2019-07-31 12:06:59 +0530

    Updated the script

--- python-scripts/changingNamesBetweenLocales.py
+++ python-scripts/changingNamesBetweenLocales.py
@@ -6,13 +6,13 @@ import codecs,cStringIO
 import io
 
 def getProperNouns(fin_fname, fout_fname):
-"""
-This function get all the possible proper nouns from the TSV file and list them in the the output file.
-fin_fname: TSV file for the proper nouns
-fout_fname: output file for the listing proper nouns(not in append mode)[*.txt file]
-Example:
-    getProperNouns('eggquizliteracy_levels_en.tsv','propernouns3.txt')
-"""
+    """
+    This function get all the possible proper nouns from the TSV file and list them in the the output file.
+    fin_fname: TSV file for the proper nouns
+    fout_fname: output file for the listing proper nouns(not in append mode)[*.txt file]
+    Example:
+        getProperNouns('eggquizliteracy_levels_en.tsv','propernouns3.txt') 
+    """
     nltk.download('punkt')
     nltk.download('averaged_perceptron_tagger')
     propernouns = set()
@@ -38,13 +38,13 @@ Example:
                 print >> f, item
 
 def mergeProperNounsFiles(fin_files, fout_file):
-"""
-This function get all the  proper nouns and generate fout_file file which will contains all the unique proper nouns
-fin_files: List of the files which contains proper nouns
-fout_file: Output file which will finally contains proper nouns(not opened in append mode)[*.txt file]
-Example:
-    mergeProperNounsFiles(["propernouns.txt", "propernouns2.txt"], 'merged_names2.txt')
-"""
+    """
+    This function get all the  proper nouns and generate fout_file file which will contains all the unique proper nouns
+    fin_files: List of the files which contains proper nouns
+    fout_file: Output file which will finally contains proper nouns(not opened in append mode)[*.txt file]
+    Example:
+        mergeProperNounsFiles(["propernouns.txt", "propernouns2.txt"], 'merged_names2.txt')
+    """
     propernouns = set()
     for file in fin_files:
         f = open(file,"r").readlines()
@@ -57,14 +57,14 @@ Example:
             print >> f, item
 
 def replaceProperNouns(tsv_in, tsv_out, map_fname):
-"""
-This function will change the names in TSV and generate a new file which will contains new names
-tsv_in: The TSV file where the names are to be changed
-tsv_out: The TSV file that will be generated which will contains the changed name
-map_fname: This file will contains tab separated two values as the mapping from the old name to new name
-Example:
-    replaceProperNouns('wordwindow_level_en.tsv', 'wordwindow_level_en_change.tsv', 'mapping.txt')
-"""
+    """
+    This function will change the names in TSV and generate a new file which will contains new names
+    tsv_in: The TSV file where the names are to be changed
+    tsv_out: The TSV file that will be generated which will contains the changed name
+    map_fname: This file will contains tab separated two values as the mapping from the old name to new name
+    Example:
+        replaceProperNouns('wordwindow_level_en.tsv', 'wordwindow_level_en_change.tsv', 'mapping.txt')
+    """
     f_indian =  io.open(map_fname,'r',encoding='utf-8').readlines()
     names_dict = dict()
     for line in f_indian:
@@ -124,8 +124,13 @@ Example:
             if length> 0:
                 for i in range(length):
                     for src_word in names_dict:
-                        cell = line[i].replace(src_word, names_dict[src_word])
-                        line[i]=cell.strip()
+                        start_chars = [u' ', u'.', u'^', u'']
+                        last_chars = [u' ',u'.', u',', u'?', u"'s"]
+                        cell = line[i]
+                        for start_char in start_chars:
+                            for last_char in last_chars:
+                                cell = cell.replace(start_char+src_word+last_char, start_char+names_dict[src_word]+last_char)
+                        line[i]=cell
                 #print line
                 #print length
                 writer.writerow(line)
